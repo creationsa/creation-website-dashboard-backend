1c4f1cd61fcdd6d256e24947ca53b6fdbd09df37
ANT0NI0US <antoniousnasr3@gmail.com>
2026-09-17 16:30:07 +0300

    solve typescrpt errors in copyrightItemsList types

--- Website/src/components/common/copyrightItemsList/types.ts
+++ Website/src/components/common/copyrightItemsList/types.ts
@@ -1,3 +1,6 @@
+import { LanguageType } from "@/i18n.config";
+import { HeaderMenuItem } from "@/lib/api/getHeader";
+
 export interface CopyrightItemsListProps {
   items: HeaderMenuItem[];
   locale: LanguageType;
